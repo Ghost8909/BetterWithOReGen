5396c6320dd510bd6041d966a663ded3058abc50
Ghost8909 <Ghost8919@Gmail.com>
2017-12-01 18:36:58 -0500

    BWOG

--- src/main/java/bwog/mod/init/ItemInit.java
+++ src/main/java/bwog/mod/init/ItemInit.java
@@ -8,6 +8,9 @@ import java.util.List;
 public class ItemInit
 {
     public static final List<Item> ITEMS = new ArrayList();
+    public static final Item POTASH_DUST = new ItemBase("dust_potash");
+    public static final Item SALT_DUST = new ItemBase("dust_salt");
     public static final Item SALTPETER_DUST = new ItemBase("dust_saltpeter");
+    public static final Item SULFUR_DUST = new ItemBase("dust_sulfur");
 }
 
